7948da3a382150f7a70453c3436243d9bc175060
Rob O'Gara <rgo594@gmail.com>
2019-03-20 14:19:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
-def collection(collection)
+def my_collect(collection)
   i = 0
+  new_collection = []
   while i < collection.length
-    yield collection[i]
-    i = i + 1
+    new_collection << yield(collection[i])
+    i += 1
   end
-  collection
+  new_collection
 end
-
